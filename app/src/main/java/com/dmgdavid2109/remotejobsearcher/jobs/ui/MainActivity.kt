235ec3ac29a894e20a654eba4328d8c19bf813c4
David Martinez Garcia <david.martinez@fundingcircle.com>
2020-07-24 11:00:09 +0100

    Moves MainActivity to UI package

--- app/src/main/java/com/dmgdavid2109/remotejobsearcher/jobs/ui/MainActivity.kt
+++ app/src/main/java/com/dmgdavid2109/remotejobsearcher/jobs/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dmgdavid2109.remotejobsearcher
+package com.dmgdavid2109.remotejobsearcher.jobs.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dmgdavid2109.remotejobsearcher.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
