6f7eff692d45c5e8e42b3519bd7eb68f706b62cb
David Martinez Garcia <david.martinez@fundingcircle.com>
2020-07-24 17:08:07 +0100

    Adds di construction to the use case

--- app/src/main/java/com/dmgdavid2109/remotejobsearcher/jobs/domain/usecase/GetCompaniesUseCase.kt
+++ app/src/main/java/com/dmgdavid2109/remotejobsearcher/jobs/domain/usecase/GetCompaniesUseCase.kt
@@ -3,8 +3,9 @@ package com.dmgdavid2109.remotejobsearcher.jobs.domain.usecase
 import com.dmgdavid2109.remotejobsearcher.common.data.Result
 import com.dmgdavid2109.remotejobsearcher.jobs.domain.model.Company
 import com.dmgdavid2109.remotejobsearcher.jobs.domain.repository.CompaniesRepository
+import javax.inject.Inject
 
-class GetAuctionsUseCase(
+class GetCompaniesUseCase @Inject constructor(
     private val companiesRepository: CompaniesRepository
 ) {
     suspend operator fun invoke(): Result<List<Company>> {
